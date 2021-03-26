b656c72b3a36b27ea0db7fa39cca2379d2c33781
Cieran0 <jimmy121212121212121212@gmail.com>
2021-03-26 18:24:31 +0000

    Kinda gamer like

--- src/cpm.c
+++ src/cpm.c
@@ -3,71 +3,79 @@
 #include <unistd.h>
 #include <string.h> 
 #include <curl/curl.h>
- 
+
+#define repoUrl "https://raw.githubusercontent.com/Cieran0/cpm-repo/main/";
+
 static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
 {
-  size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
-  return written;
+  	size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+  	return written;
 }
  
-int download(char *url, char *location)
+int download(char *pkg, char *location)
 {
-  CURL *curl_handle;
-  FILE *file; 
+	CURL *curl_handle;
+	FILE *file; 
  
-  curl_global_init(CURL_GLOBAL_ALL);
+	char url[100] = repoUrl;
+	strcat(url,pkg);
+
+  	curl_global_init(CURL_GLOBAL_ALL);
  
-  /* init the curl session */ 
-  curl_handle = curl_easy_init();
+  	/* init the curl session */ 
+  	curl_handle = curl_easy_init();
  
-  /* set URL to get here */ 
-  curl_easy_setopt(curl_handle, CURLOPT_URL, url);
+  	/* set URL to get here */ 
+  	curl_easy_setopt(curl_handle, CURLOPT_URL, url);
  
-  /* Switch on full protocol/debug output while testing  
-  curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
-  */
+  	/* Switch on full protocol/debug output while testing  
+  	curl_easy_setopt(curl_handle, CURLOPT_VERBOSE, 1L);
+  	*/
  
-  /* disable progress meter, set to 0L to enable it */
-  curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 0L);
+  	/* disable progress meter, set to 0L to enable it */
+  	curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 0L);
  
-  /* send all data to this function  */ 
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
+  	/* send all data to this function  */ 
+  	curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
  
-  /* open the file */ 
-  file = fopen(location, "wb");
-  if(file) {
+  	/* open the file */ 
+  	file = fopen(location, "wb");
+  	if(file) {
  
-    /* write the page body to this file handle */ 
-    curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, file);
+    	/* write the page body to this file handle */ 
+    	curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, file);
  
-    /* get it! */ 
-    curl_easy_perform(curl_handle);
+    	/* get it! */ 
+    	curl_easy_perform(curl_handle);
  
-    /* close the header file */ 
-    fclose(file);
-  }
+    	/* close the header file */ 
+    	fclose(file);
+  	}
  
-  /* cleanup curl stuff */ 
-  curl_easy_cleanup(curl_handle);
+  	/* cleanup curl stuff */ 
+  	curl_easy_cleanup(curl_handle);
  
-  curl_global_cleanup();
+  	curl_global_cleanup();
  
-  return 0;
+  	return 0;
 }
 
 int main(int argc, char *argsv[]) 
 {
-    char *repoUrl = " ";
 
-    if(strncmp("install",argsv[1],7) == 0)
-    {
-        download(argsv[2] , argsv[3]);
-    }
-    else if(strncmp("update",argsv[1],6) == 0)
-    {
-	printf("\n k gaymer \n");
-    }
-    else {printf("gay"); }
+	if (getuid()) {printf("%s", "You must run as root");}
+	else 
+	{
 
+    	if(strncmp("install",argsv[1],7) == 0)
+    	{
+    	    download(argsv[2] , argsv[3]);
+    	}
+    	else if(strncmp("update",argsv[1],6) == 0)
+    	{
+			printf("\n k gaymer \n");
+    	}
+    	else {printf("gay"); }
+	}
     return 0;
 }
